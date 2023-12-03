df033ae28e47e056d8d6f48b4395f4ea2fa47fe7
aoout <wuz66280@gmail.com>
2023-12-03 15:46:10 +0800

    improve rss parser.

--- src/feed.ts
+++ src/feed.ts
@@ -58,18 +58,19 @@ export default class FeedsFolder {
 			method: "GET",
 		});
 		const result = xml2js.xml2js(data);
-		const items = result.elements[0].elements[0].elements.filter(element=>element.name=="item");
+		console.log(result);
+		const items = result.elements.filter((e)=>e.name=="rss" || e.name == "feed")[0].elements[0].elements.filter(element=>element.name=="item"||element.name=="entry");
 		console.log(items);
 
 		const itemsResults = [];
 		items.forEach((item)=>{
 			const itemsResult = {};
 			item.elements.forEach((element)=>{
-				if(element.name=="title") itemsResult["title"] = element.elements[0].cdata;
-				if(element.name=="description") itemsResult["content"] = element.elements[0].cdata;
-				if(element.name=="author") itemsResult["author"] = element.elements[0].cdata;
-				if(element.name=="link") itemsResult["link"] = element.elements[0].text;
-				if(element.name=="pubDate") itemsResult["pubDate"] = element.elements[0].text;
+				if(element.name=="title") itemsResult["title"] = element.elements[0].text ?element.elements[0].text:element.elements[0].cdata ;
+				if(element.name=="description") itemsResult["content"] = element.elements[0].text ?element.elements[0].text:element.elements[0].cdata;
+				if(element.name=="author") itemsResult["author"] = element.elements[0].text ?element.elements[0].text:element.elements[0].cdata;
+				if(element.name=="link") itemsResult["link"] = element.elements[0].text ?element.elements[0].text:element.elements[0].cdata;
+				if(element.name=="pubDate") itemsResult["pubDate"] = element.elements[0].text ?element.elements[0].text:element.elements[0].cdata;
 			});
 			itemsResults.push(itemsResult);
 		});
